d0385c6b567ebda0524b1d426ebbccecb08c0f51
owayys <owais.ahsan@hotmail.com>
2025-08-26 17:21:17 +0500

    update typing

--- apps/server/src/infra/db/utils/model.utils.ts
+++ apps/server/src/infra/db/utils/model.utils.ts
@@ -1,4 +1,4 @@
-import type { UUIDType } from "@domain/utils/refined.type";
+import type { DateTimeType, UUIDType } from "@domain/utils/refined.type";
 import { timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const getPrimaryKeyCol = <T extends UUIDType = UUIDType>() =>
@@ -6,6 +6,12 @@ export const getPrimaryKeyCol = <T extends UUIDType = UUIDType>() =>
 
 export const getBaseColumns = <T extends UUIDType = UUIDType>() => ({
   id: getPrimaryKeyCol<T>(),
-  createdAt: timestamp("created_at").$type<Date>().notNull().defaultNow(),
-  updatedAt: timestamp("updated_at").$type<Date>().notNull().defaultNow(),
+  createdAt: timestamp("created_at")
+    .$type<DateTimeType>()
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .$type<DateTimeType>()
+    .notNull()
+    .defaultNow(),
 });
