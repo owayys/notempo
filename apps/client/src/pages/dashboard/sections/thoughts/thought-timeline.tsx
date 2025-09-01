9706eb32d2db54ddf94d491c1d1dbebf03993377
owayys <owais.ahsan@hotmail.com>
2025-09-01 22:44:23 +0500

    group thoughts by month

--- apps/client/src/pages/dashboard/sections/thoughts/thought-timeline.tsx
+++ apps/client/src/pages/dashboard/sections/thoughts/thought-timeline.tsx
@@ -1,19 +1,71 @@
 import { ThoughtType } from "@domain/thought/thought.schema";
-import { Box, Separator, VStack } from "@/components/ui";
+import { ChevronsUpDown } from "lucide-react";
+import { Box, HStack, Separator, VStack } from "@/components/ui";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { getMonthPretty } from "@/shared/utils/format-date";
 import { ThoughtCard } from "./thought-card";
 
 interface ThoughtTimelineProps {
   thoughts: ThoughtType[];
 }
 
-export const ThoughtTimeline = ({ thoughts }: ThoughtTimelineProps) => {
+const groupThoughtsByMonth = (
+  thoughts: ThoughtType[],
+): Record<string, ThoughtType[]> => {
+  return thoughts
+    .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+    .reduce((groups, thought) => {
+      const monthKey = getMonthPretty(thought.createdAt);
+
+      if (!groups[monthKey]) {
+        groups[monthKey] = [];
+      }
+
+      groups[monthKey].push(thought);
+
+      return groups;
+    }, {} as Record<string, ThoughtType[]>);
+};
+
+const RenderThoughts = ({ thoughts }: ThoughtTimelineProps) => {
   return (
-    <VStack className="w-full">
-      {thoughts.map((t) => (
+    <>
+      {thoughts.map((t, i) => (
         <Box className="w-full" key={t.id}>
           <ThoughtCard key={t.id} thought={t} />
-          <Separator className="my-2" variant="dashed" />
+          {i !== thoughts.length - 1 && (
+            <Separator className="my-2" variant="dashed" />
+          )}
         </Box>
+      ))}
+    </>
+  );
+};
+
+export const ThoughtTimeline = ({ thoughts }: ThoughtTimelineProps) => {
+  const groupedThoughts = groupThoughtsByMonth(thoughts);
+  const months = Object.keys(groupedThoughts);
+
+  return (
+    <VStack className="w-full">
+      <RenderThoughts thoughts={groupedThoughts[months[0]]} />
+      {months.slice(1).map((m) => (
+        <Collapsible className="w-full" key={m}>
+          <CollapsibleTrigger asChild>
+            <HStack className="w-full items-center my-2 text-primary hover:text-accent-foreground cursor-pointer transition-all duration-200">
+              {m}
+              <Separator className="flex flex-1 ml-2 mr-1.5" />
+              <ChevronsUpDown className="size-4 translate-x-0.5" />
+            </HStack>
+          </CollapsibleTrigger>
+          <CollapsibleContent>
+            <RenderThoughts thoughts={groupedThoughts[m]} />
+          </CollapsibleContent>
+        </Collapsible>
       ))}
     </VStack>
   );
