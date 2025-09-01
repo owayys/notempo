1957b73682500d20887bd273dbb71028fb6e6688
owayys <owais.ahsan@hotmail.com>
2025-09-01 20:47:43 +0500

    use breadcrumbs for dashboard header

--- apps/client/src/pages/dashboard/dashboard-header.tsx
+++ apps/client/src/pages/dashboard/dashboard-header.tsx
@@ -1,12 +1,13 @@
-import { HStack, Typography } from "@/components/ui";
+import { HStack } from "@/components/ui";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ConceptCrumbs } from "./sections/concepts/concept-crumbs";
 
 export const DashboardHeader = () => {
   return (
     <HStack className="w-full p-2 items-center justify-center">
       <SidebarTrigger className="px-3 py-2 size-9" variant="link" />
       <HStack className="w-full h-full items-center justify-center text-center">
-        <Typography variant="muted">Current Concept</Typography>
+        <ConceptCrumbs />
       </HStack>
     </HStack>
   );
---
+++ apps/client/src/pages/dashboard/sections/concepts/concept-crumbs.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export const ConceptCrumbs = () => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">Concepts</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>/</BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbPage>Some Concept</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
