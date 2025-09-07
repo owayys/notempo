82dafd714e7fc1be93551fb18e2eaca0e28e1bde
owayys <owais.ahsan@hotmail.com>
2025-09-07 20:43:01 +0500

    peel off concept-history

--- apps/client/src/pages/dashboard/sections/workspace/workspace-section.tsx
+++ apps/client/src/pages/dashboard/sections/workspace/workspace-section.tsx
@@ -1,9 +1,19 @@
+import type { ConceptType } from "@domain/concept/concept.entity";
+import type { ThoughtType } from "@domain/thought/thought.entity";
 import { ConceptMeta } from "@/components/concept/concept-meta";
 import { Typography, VStack } from "@/components/ui";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { ConceptHistory } from "../concepts/concept-history";
+import { ThoughtTimeline } from "@/pages/dashboard/sections/thoughts/thought-timeline";
 
-export const WorkspaceSection = () => {
+interface WorkspaceSectionProps {
+  concept?: ConceptType;
+  thoughts?: ThoughtType[];
+}
+
+export const WorkspaceSection = ({
+  concept,
+  thoughts,
+}: WorkspaceSectionProps) => {
   return (
     <ScrollArea className="h-full w-full items-center justify-center overflow-x-hidden">
       <VStack className="h-full w-4/5 max-w-[800px] gap-4 pt-12 m-auto">
@@ -11,10 +21,10 @@ export const WorkspaceSection = () => {
           className="font-mono text-accent-foreground w-full py-6"
           variant="h1"
         >
-          Agentic Development
+          {concept?.label}
         </Typography>
-        <ConceptMeta />
-        <ConceptHistory />
+        <ConceptMeta concept={concept} />
+        <ThoughtTimeline thoughts={thoughts} />
       </VStack>
     </ScrollArea>
   );
