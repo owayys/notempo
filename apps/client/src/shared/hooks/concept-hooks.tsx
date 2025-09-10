12ec6bb2150e5fbd949e32e1badeeb4aa7ee8a96
owayys <owais.ahsan@hotmail.com>
2025-09-11 00:02:15 +0500

    use mutation for create query + add query keys

--- apps/client/src/shared/hooks/concept-hooks.tsx
+++ apps/client/src/shared/hooks/concept-hooks.tsx
@@ -1,13 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { orpc } from "@/shared/orpc";
+import { queryClient } from "../query-client";
 
-const conceptsQueryOptions = (search?: string) => {
+const conceptsQueryOptions = (search?: string, limit?: number) => {
   return orpc.authenticated.concept.getConcept.queryOptions({
     retry: 2,
     input: {
       label: search,
+      limit,
+      sortBy: "updatedAt",
+      sortOrder: "desc",
     },
     select: (data) => data.items,
+    staleTime: 60 * 1000 * 1,
+    queryKey: [
+      "concepts",
+      {
+        label: search,
+        limit,
+      },
+    ],
   });
 };
 
@@ -21,20 +33,18 @@ const conceptDetailsQueryOptions = (id: string) => {
   });
 };
 
-const createConceptQueryOptions = (label: string) => {
-  return orpc.authenticated.concept.createConcept.queryOptions({
+const createConceptMutationOptions =
+  orpc.authenticated.concept.createConcept.mutationOptions({
     retry: 2,
-    input: {
-      label,
-    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["concepts"] }),
   });
-};
 
-export const useConcepts = (search?: string) =>
-  useQuery(conceptsQueryOptions(search));
+export const useConcepts = (search?: string, limit?: number) =>
+  useQuery(conceptsQueryOptions(search, limit));
 
 export const useConceptDetails = (id: string) =>
   useQuery(conceptDetailsQueryOptions(id));
 
-export const useCreateConcept = (label: string) =>
-  useQuery(createConceptQueryOptions(label));
+export const createConceptMutation = () => {
+  return useMutation(createConceptMutationOptions);
+};
